c736f3c45d9c29e8f0609262c116f4e48ca2332f
paili2 <zxcv7502@naver.com>
2025-03-27 17:40:19 +0900

    Add files via upload

--- socar/javascript/index.js
+++ socar/javascript/index.js
@@ -20,7 +20,6 @@ window.addEventListener("scroll", () => {
         header.classList.add("showBack");
         header.classList.remove("offBack");
         logoImg.src = "photo/logo-blue.svg";
-
         upbar.classList.add("point-color");
         upbar.classList.remove("white-color");
         downbar.classList.add("point-color");
@@ -31,20 +30,26 @@ window.addEventListener("scroll", () => {
 const hamburger = document.querySelector(".hamburger");
 const upBar = document.querySelector(".upbar");
 const downBar = document.querySelector(".downbar");
+const cell = document.querySelector(".cell-off");
 
 hamburger.addEventListener("click", () => {
-    logoImg.src = "photo/logo-blue.svg";
     if (upBar.classList.contains("up-basic")) {
+        logoImg.src = "photo/logo-blue.svg";
         upBar.classList.add("up-act");
         upBar.classList.remove("up-basic");
         upBar.classList.remove("white-color");
+        cell.classList.add("cell-on");
+        cell.classList.remove("cell-off");
         downBar.classList.add("down-act");
         downBar.classList.remove("down-basic");
         downBar.classList.remove("white-color");
     } else {
+        logoImg.src = "photo/logo-w.svg";
         upBar.classList.add("up-basic");
         upBar.classList.remove("up-act");
         upBar.classList.remove("point-color");
+        cell.classList.add("cell-off");
+        cell.classList.remove("cell-on");
         downBar.classList.add("down-basic");
         downBar.classList.remove("down-act");
         downBar.classList.remove("point-color");
@@ -64,10 +69,10 @@ hamburger.addEventListener("click", () => {
     }
 });
 
-/* app-download */
-
-// const appBox = document.querySelector(".app-box");
-// const appText = document.querySelector(".app-text");
-// const appAlbum = document.querySelector(".app-album");
-
-// window.
+// const ob = new IntersectionObserver((x) => {
+//     x.forEach((x) => {
+//         if (x.isIntersecting) {
+//             if(x.target.contains(''))
+//         }
+//     });
+// });
